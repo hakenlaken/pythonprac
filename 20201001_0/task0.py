e8735eebcd185d1c2fd2ade2819e8d4771dd4110
Andrey Larin <lav@MacBook-LAV.local>
2020-10-01 17:11:21 +0300

    commit 20201001 17:11:21 from 20201001

---
+++ 20201001_0/task0.py
@@ -0,0 +1,24 @@
+# Задача_0: поиск Парето-фронта в двумерном пространстве
+def Pareto(inp):
+    for i in range(len(inp)-1):
+        j = 0
+        while Flag := nodom(inp[i], inp[j]):
+            j += 1
+            if j == len(inp):
+                break
+        if Flag:
+            print(inp[i], " ")
+
+
+def nodom(x=(), y=()):
+    if (x[0] > y[0]) or (x[1] > y[1]):
+        return True
+    elif (x[0] <= y[0]) and (x[1] <= y[1]):
+        if x[0] >= y[0] and x[1] >= y[1]:
+            return True
+        else:
+            return False
+
+
+inp = eval(input())
+Pareto(inp)
