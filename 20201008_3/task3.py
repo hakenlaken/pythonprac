2a5a3e98e03f52d5a8ca75d540fcb20434162e3e
Andrey Larin <lav@MacBook-LAV.local>
2020-10-14 14:29:46 +0300

    commit 20201014 14:29:46 from 20201008

--- 20201008_3/task3.py
+++ 20201008_3/task3.py
@@ -1,7 +1,21 @@
-a, b = input().split()
-L = max(len(a), len(b))
-fmt = "{:>"+f"{L}"+"}"
-print(fmt)
-print(fmt.format(a))
-print(fmt.format(b))
-print()
+N, M = eval(input())
+List = list(range(1, N + 1))
+lenN = len(str(N))
+lenNN = len(str(N ** 2))
+x = int((M + 3) / (9 + 2 * len(str(N)) + len(str(N ** 2))))
+if x == 0:
+    print("too low M for such N")
+    quit()
+while len(List) > 0:
+    print("=" * M)
+    current = List[0:x]
+    if len(List) < x:
+        x = len(List)
+    for i in range(1, N + 1):
+        for j in current:
+            if j != current[len(current) - 1]:
+                print("{1:>{2}} * {0:<{2}} = {4:<{3}}".format(i, j, lenN, lenNN, i * j), end=' | ')
+            else:
+                print("{1:>{2}} * {0:<{2}} = {4:<{3}}".format(i, j, lenN, lenNN, i * j))
+    List = List[x:]
+print("=" * M)
