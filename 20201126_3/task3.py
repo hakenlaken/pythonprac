7c6bd34bbe12fceed2c350b6fab5924d5ed09772
Andrey Larin <lav@MacBook-LAV.local>
2020-12-02 22:10:21 +0300

    commit 20201202 22:10:21 добавлены тесты 20201126

--- 20201126_3/task3.py
+++ 20201126_3/task3.py
@@ -8,14 +8,8 @@ try:
     if data[:4].decode() != "RIFF" or data[8:12].decode() != "WAVE" or data[12:16].decode() != "fmt " or data[36:40].decode() != "data":
         print("NO")
         exit()
-
-    Size = struct.unpack_from("i", data, 4)[0]
-    Type = struct.unpack_from("h", data, 20)[0]
-    Channels = struct.unpack_from("h", data, 22)[0]
-    Rate = struct.unpack_from("i", data, 24)[0]
-    Bits = struct.unpack_from("h", data, 34)[0]
-    Data_size = struct.unpack_from("i", data, 40)[0]
-
+    Size, Type, Channels, Rate, Bits, Data_size = struct.unpack_from("i", data, 4)[0], struct.unpack_from("h", data, 20)[0], struct.unpack_from(
+        "h", data, 22)[0], struct.unpack_from("i", data, 24)[0], struct.unpack_from("h", data, 34)[0], struct.unpack_from("i", data, 40)[0]
     print("Size=", Size, ", Type=", Type, ", Channels=", Channels, ", Rate=",
           Rate, ", Bits=", Bits, ", Data size=", Data_size, sep="")
 except struct.error:
