079f61c76241d710142b2852e9fb72d33a0c55c5
Andrey Larin <lav@MacBook-LAV.local>
2021-02-26 18:39:24 +0300

    Sample application second commit

--- 202110220_1/task1.py
+++ 202110220_1/task1.py
@@ -1,14 +1,25 @@
+import time
 import tkinter as tk
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
         self.createWidgets()
+        self.showtime()
+
+    def showtime(self):
+        self.timeLabel["text"] = time.strftime("%c")
 
     def createWidgets(self):
+        self.timeButton = tk.Button(self, text='Time', command=self.showtime)
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.timeLabel = tk.Label(self, text='<time>')
+        self.timeButton.grid()
         self.quitButton.grid()
+        self.timeLabel.grid()
+
 
 app = Application()
 app.master.title('Sample application')
