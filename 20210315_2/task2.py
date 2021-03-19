a8c4eab87ea77fc580cd2d6a2d76f189a630252c
Andrey Larin <lav@MacBook-LAV.local>
2021-03-19 15:26:26 +0300

    final v task2.py

--- 20210315_2/task2.py
+++ 20210315_2/task2.py
@@ -3,6 +3,7 @@ Tkinter skeleton app
 '''
 import tkinter as tk
 from itertools import product
+import re
 
 
 class Application(tk.Frame):
@@ -23,13 +24,44 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         '''Create all the widgets'''
-        self.B = tk.Button(self, text="Quit", command=self.master.quit)
-        self.B.bind('<Any-KeyPress>', lambda event: print(event))
-        self.master.bind('<Any-KeyPress>', lambda event: print("Root", event))
-        self.B.grid()
-        self.E = tk.Button(self, text="Event",
-                           command=lambda: self.event_generate('<KeyPress-Return>'))
-        self.E.grid()
+        self.choice = tk.StringVar()
+        self.S = tk.StringVar()
+        self.S.set("Default")
+
+        self.SforE = tk.StringVar()
+        self.SforE.set("")
+        self.pattern = re.compile(r"^\w{0,10}$")
+        vcmd = (self.register(self.validate_word), "%i", "%P")
+        self.E = tk.Entry(self, validate="key",
+                          validatecommand=vcmd,
+                          invalidcommand=self.print_error, textvariable=self.SforE)
+        self.E.grid(row=0, columnspan=3)
+
+        self.optionlist = ('One', 'Two', 'Three')
+        self.choice.set(self.optionlist[0])
+        self.OM = tk.OptionMenu(self, self.choice, *self.optionlist)
+        self.QB = tk.Button(self, text="Quit", command=self.master.quit)
+        # self.master.bind('<Any-KeyPress>', lambda event: print("Root", event))
+        self.OM.grid(row=1, column=0, sticky="W")
+        self.L = tk.Label(self, textvariable=self.S)
+        self.L.bind('<Enter>', lambda event: self.S.set("Hi Mouse"))
+        self.L.bind('<Leave>', lambda event: self.S.set("Bye Mouse"))
+        self.L.grid(row=0, column=3, columnspan=2)
+        self.QB.grid(row=1, column=4)
+        self.IB = tk.Button(self, text="Insert",
+                            command=lambda: self.E.insert(tk.END, self.choice.get()))
+        self.IB.grid(row=1, column=2, sticky="E")
+        self.SB = tk.Button(self, text="Show", command=lambda: self.S.set(self.SforE.get()))
+        self.SB.grid(row=1, column=3)
+
+    def validate_word(self, index, username):
+        '''entry validation'''
+        # print("Modification at index " + index)
+        return self.pattern.fullmatch(username) is not None
+
+    def print_error(self):
+        '''error validation'''
+        # print("Invalid username character")
 
 
 class App(Application):
@@ -37,5 +69,5 @@ class App(Application):
         super().create_widgets()
 
 
-app = App(title="Sample application")
+app = App(title="task2.py")
 app.mainloop()
